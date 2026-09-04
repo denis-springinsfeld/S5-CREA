93299b83cf6016d882c36a3fe4b6cafed2a1c8fd
denis-springinsfeld <denis.springinsfeld@unilim.fr>
2026-09-04 15:50:52 +0200

    page

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://denis-springinsfeld.github.io/SR-CREA/",
+  base: "https://denis-springinsfeld.github.io/S5-CREA/",
 });
